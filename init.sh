872dc66be7e9d963e970774404306ef311acc60d
sudosu404 <mayayx@proton.me>
2025-08-13 23:36:55 +0000

    basic clod github setup

--- init.sh
+++ init.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# Verifica dependencias
-for cmd in curl wget install; do
-  command -v $cmd >/dev/null || { echo "$cmd requerido"; exit 1; }
-done
 
 # Descarga ISO de Proxmox
 mkdir -p /tmp/.storage
